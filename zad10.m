97988ad3d43f50f99b91345cbd61a6f928c2324f
kuba <J.Sikora@stud.elka.pw.edu.pl>
2018-04-02 23:30:44 +0200

    Dodane opisy do modeli w simulinku

--- zad10.m
+++ zad10.m
@@ -9,11 +9,13 @@ u_lin = 1;
 
 syms s u_lin 
 
+% Model zlinearyzowany 
 A = [-(T1+T2)/(T1*T2) 1; -1/(T1*T2) 0];
 B = [0; (K*(a1 + 2*a2*u_lin + 3*a3*(u_lin^2) + 4*a4*(u_lin^3)))/(T1*T2)];
 C = [1 0];
 D = 0;
 
+% Wzór na transmitancje
 G = C*((s*eye(2) - A)^-1)*B;
 
 disp('Transmitancja uzyskana z ci¹g³ego modelu dynamicznego zlinearyzowanego (pkt. lin u = 1): ');
