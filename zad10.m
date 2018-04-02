6da22fcd8c69eae23339c7486f9efc20670d307e
kuba <J.Sikora@stud.elka.pw.edu.pl>
2018-04-02 22:06:49 +0200

    Male poprawki

--- zad10.m
+++ zad10.m
@@ -2,32 +2,22 @@
 % Skrypt wyznaczaj¹cy transmitancjê zlinearyzowanego modelu dyskretnego w
 % punkkcie pracy u_lin
 
-clear;
-syms a1 a2 a3 a4 K T1 T2 Tp pkt_pracy z G
+% Parametry modelu w przestrzeni stanu ci¹g³e
+% A = [(T1*T2 - (T1+T2)*Tp)/(T1*T2) Tp; -Tp/(T1*T2) 1];
+% B = [0; (K*Tp*(a1 + 2*a2*pkt_pracy + 3*a3*pkt_pracy^2 + 4*a4*pkt_pracy^3))/(T1*T2)];
+% C = [1 0];
+% D = 0;
 
-% Parametry modelu w przestrzeni stanu
-A = [(T1*T2 - (T1+T2)*Tp)/(T1*T2) Tp; -Tp/(T1*T2) 1];
-B = [0; (K*Tp*(a1 + 2*a2*pkt_pracy + 3*a3*pkt_pracy^2 + 4*a4*pkt_pracy^3))/(T1*T2)];
+A = [(-T1 - T2)/(T1*T2) 1; -1/(T1*T2) 0];
+B = [0; a1 + 2*a2*u_lin + 3*a3*(u_lin^2) + 4*a4*(u_lin^3)];
 C = [1 0];
 D = 0;
 
-% Wzór na transmitancjê
-G = C*(inv(z*eye(2) - A))*B + D;
+sys = ss(A,B,C,D,Tp)
 
-disp('Transmitancja dyskretna parametryzowana G(z):');
-disp(G);
+step(sys)
 
-% Podstawienie parametrów modelu
-set_params;
-Tp = 1;
-pkt_pracy = 1;
-syms G z
-G = (K*Tp^2*(4*a4*pkt_pracy^3 + 3*a3*pkt_pracy^2 + 2*a2*pkt_pracy + a1))/(Tp^2 - T2*Tp - T1*Tp - T1*T2*z + T1*Tp*z + T2*Tp*z + T1*T2*z^2);
-disp('Postaæ z podstawionymi parametrami: ');
-disp(G);
 
-% % Model dyskretny
-% model = tf((K*Tp^2*(4*a4*(pkt_pracy^3) + 3*a3*(pkt_pracy^2) + 2*a2*pkt_pracy + a1))/(T1*T2), [(T1*T2) (2*T1*T2 + T1*Tp + T2*Tp) (T1*T2 - T1*Tp - T2*Tp + Tp)], Tp);
-% % wzm1
-% step(model, 1000)
-% %disp(resp(1000));
+t = tf(sys)
+
+
