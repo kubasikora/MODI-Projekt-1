6dadf645d489941e7c2a4d3850f3cea3abdd9efe
kuba <J.Sikora@stud.elka.pw.edu.pl>
2018-04-16 21:50:52 +0200

    Finished project

--- scripts/execute_all.m
+++ scripts/execute_all.m
@@ -1,5 +1,7 @@
 % MODI - Projekt 1 - Zadanie 36 - Jakub Sikora
-% Uruchamia wszystkie skrypty generuj¹ce wykresy
+% Uruchamia wszystkie skrypty generuj¹ce wykresy i reprezentacje graficzn¹
+% systemów 
+
 
 zad3;
 zad4;
@@ -32,6 +34,28 @@ print('-szad1v2', 'figures\zad1\system', '-dpdf', '-r400')
 cd(folder)
 bdclose 
 
+% Otwórz reprezentacje graficzna zadania 2
+open_system('zad2disc');
+set_param('zad2disc', 'PaperType', 'usletter');
+set_param('zad2disc', 'PaperOrientation', 'landscape');
+set_param('zad2disc', 'PaperPositionMode', 'manual');
+set_param('zad2disc', 'PaperPosition', [0.5 0.5 4 4]);
+folder = pwd();
+cd('../')
+
+% Stwórz folder na wykresy jeœli jeszcze nie istnieje
+    if(exist('figures', 'dir') == 0)
+        mkdir('figures'); 
+    end
+    
+    if(exist('figures\zad2', 'dir') == 0)
+        mkdir('figures\zad2'); 
+    end
+    
+% Zapisz model
+print('-szad2disc', 'figures\zad2\system', '-dpdf', '-r400')
+cd(folder)
+bdclose 
 
 % Otwórz reprezentacje graficzna zadania 8
 open_system('zad8');
--- scripts/zad12.m
+++ scripts/zad12.m
@@ -4,41 +4,59 @@
 
 clear;
 set_params;
-u_lin = 1;
+u_lin = 0.5;
 
 % Symulacja
 sim('zad12_test')
 
 % Wykres b³êdny
-fig = figure('Name', 'Porównanie transmitancji i modelu w przestrzeni stanów 1');
+fig = figure('Name', strcat('Porównanie transmitancji i modelu w przestrzeni stanów 1, u = ', num2str(u_lin)));
 set(gcf, 'Position', [100, 100, 800, 600])
 hold on;
 grid on;
 grid minor;
 plot(nonlin_out_G);
 plot(nonlin_out_C);
-legend('Transmitancja', 'Model w przestrzeni stanów (z sta³ymi)', 'Location', 'northeast');
-title('Porównanie transmitancji i modelu w przestrzeni stanów 1', 'FontName', 'Helvetica');
+legend('Transmitancja', 'Model w przestrzeni stanów (z sta³ymi)', 'Location', 'southwest');
+title(strcat('Porównanie transmitancji i modelu w przestrzeni stanów 1, u = ', num2str(u_lin)), 'FontName', 'Helvetica');
 xlabel('Wartoœæ sterowania u');
 ylabel('Wyjœcie modelu y');
-
-
-name = 'figures\zad12\porownanie1';
+name = 'figures\zad12\porownanie7';
 print_figure(name, 'figures\zad12')
 
+
 % Wykres poprawny
-fig = figure('Name', 'Porównanie transmitancji i modelu w przestrzeni stanów 2');
+fig = figure('Name', strcat('Porównanie transmitancji i modelu w przestrzeni stanów 2, u = ', num2str(u_lin)));
 set(gcf, 'Position', [100, 100, 800, 600])
 hold on;
 grid on;
 grid minor;
 plot(nonlin_out_G);
 plot(nonlin_out_M);
-legend('Transmitancja', 'Model w przestrzeni stanów (bez sta³ych)', 'Location', 'northeast');
-title('Porównanie transmitancji i modelu w przestrzeni stanów 2', 'FontName', 'Helvetica');
+legend('Transmitancja', 'Model w przestrzeni stanów (bez sta³ych)', 'Location', 'southwest');
+title(strcat('Porównanie transmitancji i modelu w przestrzeni stanów 2, u = ', num2str(u_lin)), 'FontName', 'Helvetica');
 xlabel('Wartoœæ sterowania u');
 ylabel('Wyjœcie modelu y');
 
 % Zapis wykresu do pliku
-name = 'figures\zad12\porownanie2';
+name = 'figures\zad12\porownanie8';
+print_figure(name, 'figures\zad12')
+
+
+% Wykres testowy
+fig = figure('Name', strcat('Porównanie transmitancji i modelu w przestrzeni stanów 3, u_ = ', num2str(u_lin)));
+set(gcf, 'Position', [100, 100, 800, 600])
+hold on;
+grid on;
+grid minor;
+plot(nonlin_out_G2);
+plot(nonlin_out_C);
+legend('Zmodyfikowana transmitancja', 'Model w przestrzeni stanów (z sta³ymi)', 'Location', 'southwest');
+title(strcat('Porównanie transmitancji i modelu w przestrzeni stanów 3, u = ', num2str(u_lin)), 'FontName', 'Helvetica');
+xlabel('Wartoœæ sterowania u');
+ylabel('Wyjœcie modelu y');
+
+
+% Zapis wykresu do pliku
+name = 'figures\zad12\porownanie9';
 print_figure(name, 'figures\zad12')
